22422ab799d6d3c6ae0f38c968dfe8d5731b7990
MRKS77 <artxmarkos@gmail.com>
2021-08-05 04:26:33 +0300

    init: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let mrks = [];
+    if (!matrix) return mrks;
+    let toggle = true;
+    matrix.forEach((e) => {
+        toggle ? (mrks = [...mrks, ...e]) : (mrks = [...mrks, ...e.reverse()]);
+        toggle = !toggle;
+    });
+    return mrks;
+};
